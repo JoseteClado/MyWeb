3095bbc00320ffefbc70bdfa744a0717549feab9
Jose Paredes <joseparedes@estudiante.edib.es>
2022-03-21 14:47:13 +0100

    Primera web

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+
+
+        $(document).ready(function(){
+            $('.img1').hover(function() {
+                $(this).addClass('transition');
+            }, function() {
+                $(this).removeClass('transition');
+            });
+        });
+ 
